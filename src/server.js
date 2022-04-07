7f038ff4153e0f257a9202f07ee0e1e9c6eaa89b
bruno-nakahara <Bruno_Nakahara95@yahoo.com>
2022-04-06 23:49:51 -0300

    add swagger

--- src/server.js
+++ src/server.js
@@ -2,7 +2,11 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import swaggerUi from 'swagger-ui-express';
+
 import eventRoutes from './routes/eventRoutes';
+
+import swaggerDocs from './swagger.json';
 
 require('dotenv').config();
 const app = express();
@@ -13,7 +17,16 @@ mongoose.connect(process.env.MONGO_DB_URL, {
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use(eventRoutes);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+app.get('/terms', (req, res) => {
+  return res.json({
+    message: 'Termos de Serviço',
+  });
+});
+
+app.use('/v1', eventRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log(`Rodando na porta ${process.env.PORT}`);
